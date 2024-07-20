532f3824fc19a87bf351f6233c37167a5d380fd1
jjone895 <jjone895@students.kennesaw.edu>
2024-07-20 19:28:17 -0400

    Update LEDButton.py
    Added the on/off button and added a title

--- LEDButton.py
+++ LEDButton.py
@@ -9,23 +9,21 @@ def button2_click():
 def button3_click():
     print("Button 3 clicked")
 
+def toggle_on_off():
+    if on_off_button.config('text')[-1] == 'Off':
+        on_off_button.config(text='On')
+        print("Turned On")
+    else:
+        on_off_button.config(text='Off')
+        print("Turned Off")
+
 # Create the main window
 root = tk.Tk()
 root.title("Three Buttons")
 
-# Create buttons and assign them to the functions
-button1 = tk.Button(root, text="Button 1", command=button1_click)
-button2 = tk.Button(root, text="Button 2", command=button2_click)
-button3 = tk.Button(root, text="Button 3", command=button3_click)
-
-# Pack the buttons into the window
-button1.pack(pady=10)
-button2.pack(pady=10)
-button3.pack(pady=10)
-
 # Set the window size
 window_width = 300
-window_height = 200
+window_height = 300
 
 # Get the screen dimensions
 screen_width = root.winfo_screenwidth()
@@ -37,6 +35,24 @@ position_y = (screen_height // 2) - (window_height // 2)
 
 # Set the geometry of the window
 root.geometry(f'{window_width}x{window_height}+{position_x}+{position_y}')
+
+# Create a header label
+header = tk.Label(root, text="Illuminating Connectivity", font=("Arial", 16))
+header.pack(pady=10)
+
+# Create buttons and assign them to the functions
+button1 = tk.Button(root, text="Button 1", command=button1_click)
+button2 = tk.Button(root, text="Button 2", command=button2_click)
+button3 = tk.Button(root, text="Button 3", command=button3_click)
+
+# Pack the buttons into the window
+button1.pack(pady=10)
+button2.pack(pady=10)
+button3.pack(pady=10)
+
+# Create an On/Off button
+on_off_button = tk.Button(root, text="Off", command=toggle_on_off)
+on_off_button.pack(pady=10)
 
 # Run the application
 root.mainloop()
